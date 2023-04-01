9ecf7a8b37e4c86a8d912dcaea3773b21a69f0e4
Sjskyler27Laptop <sjskyler27@gmail.com>
2023-04-01 15:06:00 -0600

    add dice func

--- DUNETDMF/scripts/app.js
+++ DUNETDMF/scripts/app.js
@@ -20,7 +20,50 @@ class Dice {
       this.locked = !this.locked;
     }
   }
+
+  // function to add dice to the Array, takes a name and Number, 
+  // number could be any realy as long as it is unique
+  function addDice(type,num) {
+        const dice = new Dice(`${type}${num}`, `./dice/${type}/`);
+        diceArray.push(dice);
+
+        // create the dice element and add it to the container
+        const diceElement = document.createElement('div');
+        diceElement.className = 'dice';
+        diceElement.id = `${type}${num}`;
+        // make the first image you see 1
+        diceElement.style.backgroundImage = `url(./dice/${type}/1.png)`;
+        diceContainer.appendChild(diceElement);
+
+        // add a listener for click on each dice.
+        diceElement.addEventListener('click', () => {
+          dice.toggleLock();
+          diceElement.classList.toggle('locked');
+        });
+
+
+        // //listen for long press on a dice to display options
+        // const longPressDuration = 500; // duration in milliseconds
+
+        // diceElement.addEventListener('touchstart', () => {
+        //   this.timeoutId = setTimeout(() => {
+        //     showOptionsForDice(dice);
+        //   }, longPressDuration);
+        // });
+
+        // diceElement.addEventListener('touchend', () => {
+        //   if (this.timeoutId) {
+        //     clearTimeout(this.timeoutId);
+        //   }
+        // });
+
+        // function showOptionsForDice(dice) {
+        //   // display options for the dice
+        //   console.log('options are:');
+        // }
+  }
   
+  //displays the new face for each dice in the array
   function rollDice() {
     diceArray.forEach((dice) => {
         if (!dice.locked) {
@@ -60,22 +103,23 @@ unlockButton.addEventListener("click", () => {
       dice.toggleLock();
       const diceElement = document.getElementById(dice.name.toLowerCase());
       diceElement.classList.remove("locked");
-          // const diceContainer = diceElement.parentElement;
-          // diceContainer.classList.remove("locked");
       }
     
   });
 });
+
+
       
 /**************************
  * MAIN
  ***************************/
     
-// set up an array to store the dice
+// set up an global array to store the dice
 const diceArray = [];
 
 // grab the container element
 const diceContainer = document.getElementById('dicecontainer');
+
 
 // fetch the dice data from a JSON file
 fetch('./dice/dune.json')
@@ -85,22 +129,7 @@ fetch('./dice/dune.json')
     data.forEach(({ type, count }) => {
 
       for (let i = 1; i <= count; i++) {
-        const dice = new Dice(`${type}${i}`, `./dice/${type}/`);
-        diceArray.push(dice);
-
-        // create the dice element and add it to the container
-        const diceElement = document.createElement('div');
-        diceElement.className = 'dice';
-        diceElement.id = `${type}${i}`;
-        diceElement.style.backgroundImage = `url(./dice/${type}/1.png)`;
-        diceContainer.appendChild(diceElement);
-
-        // add a listener for click on each dice.
-        diceElement.addEventListener('click', () => {
-          dice.toggleLock();
-          diceElement.classList.toggle('locked');
-          // diceContainer.classList.toggle('locked', dice.locked);
-        });
+        addDice(type,i)
       }
 
     });
@@ -111,6 +140,31 @@ fetch('./dice/dune.json')
 
 
 
+// add a listener for long-press on each dice
+diceArray.forEach((dice) => {
+  const diceElement = document.getElementById(dice.name.toLowerCase());
+  let longPressTimeout;
+
+  diceElement.addEventListener("mousedown", () => {
+    longPressTimeout = setTimeout(() => {
+      // show modal dialog to ask user if they want to remove the dice
+      const removeDice = confirm("Remove this dice?");
+      if (removeDice) {
+        // remove the dice element and corresponding dice object from the array
+        diceContainer.removeChild(diceElement);
+        diceArray.splice(diceArray.indexOf(dice), 1);
+      }
+    }, 1000); // set the long-press timeout to 1 second
+  });
+
+  diceElement.addEventListener("mouseup", () => {
+    clearTimeout(longPressTimeout);
+  });
+
+  diceElement.addEventListener("mouseleave", () => {
+    clearTimeout(longPressTimeout);
+  });
+});
 
 
 
